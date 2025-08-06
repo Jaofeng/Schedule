2da6ecfb17f0a3853a1fed4226d3db7b7c51fc62
怪頭 <jaofeng.chen@gmail.com>
2025-08-06 15:04:35 +0800

    修正 PlanWorkerOptions.cs 中的 XML 註解格式，確保語法正確

--- CJF.Schedule/PlanWorkerOptions.cs
+++ CJF.Schedule/PlanWorkerOptions.cs
@@ -9,7 +9,7 @@ public sealed class PlanWorkerOptions
     /// <summary><see cref="PlanWorker"/> 排程項目檢查週期的間隔時間，單位秒。</summary>
     public int Interval { get; set; } = 30;
     /// <summary>是否自動綁定具有 <see cref="PlanAttribute"/> 的靜態方法。</summary>
-    /// <remarks>如果為 <see langword="true">，則會在建構 <see cref="PlanWorker"/> 時自動綁定所有符合條件的方法。</remarks>
+    /// <remarks>如果為 <see langword="true"/>，則會在建構 <see cref="PlanWorker"/> 時自動綁定所有符合條件的方法。</remarks>
     public bool AutoBind { get; set; } = true;
 }
 
