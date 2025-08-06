a04d648728bea36bf5bf26aeb4c70eb62233438b
怪頭 <jaofeng.chen@gmail.com>
2025-08-06 16:06:12 +0800

    補上說明

--- CJF.Schedule/_Exceptions.cs
+++ CJF.Schedule/_Exceptions.cs
@@ -2,6 +2,10 @@
 namespace CJF.Schedules;
 
 #region Public Class : KeyExistsException
+/// <summary>當嘗試添加已存在的鍵時拋出此異常。</summary>
+/// <param name="key">已存在的鍵。</param>
+/// <param name="msg">異常消息。</param>
+/// <remarks>此異常用於表示嘗試添加的鍵已經存在於集合中。</remarks>
 public class KeyExistsException(string key, string msg) : Exception(msg)
 {
     public string Key { get; private set; } = key;
@@ -9,6 +13,9 @@ public class KeyExistsException(string key, string msg) : Exception(msg)
 #endregion
 
 #region Public Class : ExceptionEventArgs
+/// <summary>表示異常事件的參數。</summary>
+/// <param name="ex">引發的異常。</param>
+/// <remarks>此類用於在異常發生時傳遞相關的異常信息。</remarks>
 public class ExceptionEventArgs(Exception ex) : EventArgs()
 {
     public Exception Exception { get; private set; } = ex;
